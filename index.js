c56482f34982ce7b15a05973f3fd7698a00fc8c2
Leandro Tavares <lgomt@hotmail.com>
2023-05-19 17:45:25 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+console.log('iniciando')
+
+
+function iniciarExpedientes() {
+   //padaria 1
+   
+   //padeiro
+    millerPrepararMassa()
+    millerAssarMassa()
+    millerColocarPaesNaPrateleira()
+
+    //confeiteiro
+    maikPegarIngredientes()
+    maikBaterBolos()
+    maikAssarBolos()
+
+    //caixa
+    leandroSepararTroco()
+    leandroAbrirCaixa()
+
+   //padaria 2
+    
+   //padeiro
+   marquinhoPrepararMassa()
+   marquinhoAssarMassa()
+   marquinhoColocarPaesNaPrateleira()
+
+   //confeiteiro
+   chicoPegarIngredientes()
+   chicoBaterBolos()
+   chicoAssarBolos()
+
+   //caixa
+   marliSepararTroco()
+   marliAbrirCaixa()
+}
+
+//padeiro
+function millerPrepararMassa() {
+    console.log('preparando massa')
+}
+function millerAssarMassa() {
+    console.log('assando massa')
+}
+function millerColocarPaesNaPrateleira() {
+    console.log('colocando paes na prateleira')
+}
+
+//confeiteiro
+function maikPegarIngredientes() {
+    console.log('pegando ingredientes')
+}
+function maikBaterBolos() {
+    console.log('batendo bolos')
+}
+function maikAssarBolos() {
+    console.log('preparando mas')
+}
+
+//caixa
+function leandroSepararTroco() {
+    console.log('preparando mas')
+}
+function leandroAbrirCaixa() {
+    console.log('abrindo caixa')
+}
+
+//padeiro
+function marquinhoPrepararMassa() {
+    console.log('preparando massa')
+}
+function marquinhoAssarMassa() {
+    console.log('assando massa')
+}
+function marquinhoColocarPaesNaPrateleira() {
+    console.log('colocando paes na prateleira')
+}
+
+//confeiteiro
+function chicoPegarIngredientes() {
+    console.log('pegando ingredientes')
+}
+function chicoBaterBolos() {
+    console.log('batendo bolos')
+}
+function chicoAssarBolos() {
+    console.log('assando bolos')
+}
+
+//caixa
+function marliSepararTroco() {
+    console.log('separando troco')
+}
+function marliAbrirCaixa() {
+    console.log('abrindo caixa')
+}
+
+iniciarExpedientes()
